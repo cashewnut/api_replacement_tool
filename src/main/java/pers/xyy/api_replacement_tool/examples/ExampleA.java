4764d90bdac81b37d80427847697d20059e258aa
cashewnut <xiyaoguo@yeah.net>
2019-06-06 18:52:20 +0800

    modify

--- src/main/java/pers/xyy/api_replacement_tool/examples/ExampleA.java
+++ src/main/java/pers/xyy/api_replacement_tool/examples/ExampleA.java
@@ -6,23 +6,23 @@ import javax.swing.text.View;
 import java.util.Date;
 import javax.swing.JScrollPane;
 
-public class C {
+public class ExampleA {
 
     public void testMethod(String str, JComponent jp) {
         System.out.println(str);
         Date date = new Date();
         int month = 5;
         date.setMonth(5);
-        long time = date.UTC(2017, 3, 3, 3, 3, 3);
+        long time = date.UTC(2017,3,3,3,3,3);
         int year = date.getYear();
         int timezoneoffset = date.getTimezoneOffset();
         System.out.println(getMonth(""));
         JMenuBar menuBar = new JMenuBar();
         menuBar.getComponentAtIndex(5);
-        View view = new BoxView(null, 5);
-        view.viewToModel(1, 1, null);
+        View view = new BoxView(null,5);
+        view.viewToModel(1,1,null);
         JTable jTable = new JTable();
-        JScrollPane jScrollPane = JTable.JScrollPane(jTable);
+        JScrollPane jScrollPane = JTable.createScrollPaneForTable(jTable);
     }
 
     public int getMonth(String str) {
